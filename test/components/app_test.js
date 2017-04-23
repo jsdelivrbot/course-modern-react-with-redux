3bfdf576e4ff2ee8190382c34d587c049eb49485
Peter Donald <peter@realityforge.org>
2017-04-23 13:47:51 +1000

    Code style

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,14 @@
-import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
+import { renderComponent } from '../test_helper';
 
-describe('App' , () => {
+describe( 'App', () => {
   let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+  beforeEach( () => {
+    component = renderComponent( App );
+  } );
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+  it( 'renders something', () => {
+    expect( component ).to.exist;
+  } );
+} );
