73240fe350214380897d617c12c99cdd63330d23
Peter Donald <peter@realityforge.org>
2017-04-27 06:50:57 +1000

    Load videos from youtub in App component

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
 
+const YOUTUBE_API_KEY = 'AIzaSyDVHlwVTrJu4cstb1ArMRd7JhLKHL7wruY';
+
 export default class App extends Component {
+  constructor( props ) {
+    super( props );
+
+    this.state = { videos: [] };
+    YTSearch( { key: YOUTUBE_API_KEY, term: 'musictheory' }, videos => this.setState( { videos } ) );
+  }
+
   render() {
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-const YOUTUBE_API_KEY = 'AIzaSyDVHlwVTrJu4cstb1ArMRd7JhLKHL7wruY';
-
 ReactDOM.render( <App />, document.querySelector( '.container' ) );
