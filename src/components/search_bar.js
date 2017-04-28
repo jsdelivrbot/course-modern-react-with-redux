57f054ea9cf9e7d61db2eba9a135c4f94f59ff8c
Peter Donald <peter@realityforge.org>
2017-04-28 20:43:51 +1000

    Decruft

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,19 +1,5 @@
 import React, { Component } from 'react';
 
-/**
- * A functional component as it takes input and produces output that is a function of input.
- * It contains no state.
- */
-/*
- const SearchBar_old = () => {
- return <input/>;
- };
- export default SearchBar;
- */
-
-/**
- * A stateful class based component.
- */
 class SearchBar extends Component {
   constructor( props ) {
     super( props );
@@ -27,7 +13,6 @@ class SearchBar extends Component {
         <input
           value={this.state.term}
           onChange={e => this.setState( { term: e.target.value } )}/>
-        <p>The search term '{this.state.term}'</p>
       </div>
     );
   }
