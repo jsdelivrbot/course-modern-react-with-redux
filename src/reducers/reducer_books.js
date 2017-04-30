5a023d3c5979c184e4174863d195effd3b338732
Peter Donald <peter@realityforge.org>
2017-04-30 14:37:46 +1000

    Remove extract data

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,15 +3,6 @@ export default function () {
     { title: 'Harry Potter', pages: 22 },
     { title: 'Star Wars', pages: 102 },
     { title: 'Indianna Jones', pages: 129 },
-    { title: 'The Goonies', pages: 34 },
-    { title: 'Book 1', pages: 1 },
-    { title: 'Book 2', pages: 2 },
-    { title: 'Book 3', pages: 3 },
-    { title: 'Book 4', pages: 4 },
-    { title: 'Book 5', pages: 5 },
-    { title: 'Book 6', pages: 6 },
-    { title: 'Book 7', pages: 7 },
-    { title: 'Book 8', pages: 8 },
-    { title: 'Book 9', pages: 9 }
+    { title: 'The Goonies', pages: 34 }
   ];
 }
