93ad0d7541ae4b35eac7ab74e05540c2ea14fc0c
Peter Donald <peter@realityforge.org>
2017-04-30 14:07:50 +1000

    Hook container upto actions

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { selectBook } from '../actions/action_select_book';
 
 class BookList extends Component {
   render() {
@@ -11,14 +13,30 @@ class BookList extends Component {
   }
 
   renderList() {
-    return this.props.books.map( book => <li key={book.title} className="list-group-item">{book.title}</li> );
+    return this.props.books.map( book =>
+                                   <li key={book.title}
+                                       className="list-group-item"
+                                       onClick={() => this.props.selectBook( book )}>
+                                     {book.title}
+                                   </li> );
   }
 }
 
+// Whatever is returned will show up as props on the BookList container
 function mapStateToProps( state ) {
   return {
     books: state.books
   };
 }
 
-export default connect( mapStateToProps )( BookList );
+// Whatever is returned will show up as props on the BookList container
+// i.e. this.props.selectBook will be available in the BookList
+function mapDispatchToProps( dispatch ) {
+  // Whenever selectBook is called the result should be passed to all our reducers
+  // This is the purpose of bindActionCreators.
+  return bindActionCreators( { selectBook: selectBook }, dispatch );
+}
+
+// Promote BookList from a component to a container. It provides state from redux as props
+// and actions from redux as props.
+export default connect( mapStateToProps, mapDispatchToProps )( BookList );
