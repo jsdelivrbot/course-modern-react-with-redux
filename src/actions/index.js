3f98aed719fe8411cfbf803ea2b9ee5e90a6de95
Peter Donald <peter@realityforge.org>
2017-05-01 21:39:42 +1000

    Fix api URL

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '5834dbb3197d5d03e4e560aef3556ca2';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 //Use a constant to avoid isssues with copy pasting strings
 export const FETCH_WEATHER = 'FETCH_WEATHER';
