53945f06ecbadd41108b0ac425f466243d0151be
Peter Donald <peter@realityforge.org>
2017-05-01 21:26:07 +1000

    Add FETCH_WEATHER action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = '5834dbb3197d5d03e4e560aef3556ca2';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+//Use a constant to avoid isssues with copy pasting strings
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather( city ) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  //Request is a promise that is processed by ReduxPromise middleware
+  const request = axios.get( url );
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
