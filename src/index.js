02cb483cc768d7ba4ae6d1ac35595a7ec11904e3
Peter Donald <peter@realityforge.org>
2017-05-13 12:24:08 +1000

    Initial simplified routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './components/app';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()( createStore );
 
+class Hello extends React.Component {
+  render() {
+    return (<div>Hello!</div>);
+  }
+}
+
+class Goodbye extends React.Component {
+  render() {
+    return (<div>Bye!</div>);
+  }
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  <Provider store={createStoreWithMiddleware( reducers )}>
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello}/>
+        <Route path="/goodbye" component={Goodbye}/>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector( '.container' ) );
