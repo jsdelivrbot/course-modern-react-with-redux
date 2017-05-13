2dbf5eac1a129d1cc6eb6c7cda67fee19566cf84
Peter Donald <peter@realityforge.org>
2017-05-13 14:11:54 +1000

    Add new location for creating post

---
+++ src/containers/posts/create_post.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class CreatePost extends Component {
+  render() {
+    return (<h1>Create Post</h1>);
+  }
+}
+
+export default CreatePost;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
 import ReduxPromise from 'redux-promise';
+import CreatePost from './containers/posts/create_post';
 import ListPosts from './containers/posts/list_posts';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware( ReduxPromise )( createStore );
 
+// Note that <Switch> will pick the first route that matches the url
+// So more specific routes should occur earlier in order.
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware( reducers )}>
     <BrowserRouter>
       <div>
+        <Switch>
+          <Route path="/posts/new" component={CreatePost}/>
           <Route path="/" component={ListPosts}/>
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
