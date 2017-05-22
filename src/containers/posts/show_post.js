82a1deca934c578291d874792625b6c3af5c3cee
Peter Donald <peter@realityforge.org>
2017-05-22 22:58:07 +1000

    Fetch data for specific post

--- src/containers/posts/show_post.js
+++ src/containers/posts/show_post.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { fetchPost } from '../../actions/index';
 
 class ShowPost extends Component {
+  componentDidMount() {
+    // This prop is provided by react-router-dom and
+    // is extracted from :id segment in router
+    const postID = this.props.match.params.id;
+
+    this.props.fetchPost( postID );
+  }
+
   render() {
     return (
       <h1>Hi</h1>
