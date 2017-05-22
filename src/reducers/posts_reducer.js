02de98018de43ebe961b356b100f30fac308a0c9
Peter Donald <peter@realityforge.org>
2017-05-23 08:27:36 +1000

    Avoid dereference error before state loaded

--- src/reducers/posts_reducer.js
+++ src/reducers/posts_reducer.js
@@ -5,11 +5,16 @@ export default function ( state = {}, action ) {
   switch ( action.type ) {
     case FETCH_POST:
       const post = action.payload.data;
-      return { ...state, [post.id]: post };
-      // The following code matched the above line in functionality
-      //const newState = { ...state };
-      //newState[ post.id ] = post;
-      //return newState;
+      if ( post ) {
+        return { ...state, [post.id]: post };
+        // The following code matched the above line in functionality
+        //const newState = { ...state };
+        //newState[ post.id ] = post;
+        //return newState;
+      }
+      else {
+        return state;
+      }
     case FETCH_POSTS:
       const posts = action.payload.data;
       return _.mapKeys( posts, 'id' );
